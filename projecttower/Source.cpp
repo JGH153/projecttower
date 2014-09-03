e71877a9690213beb6d0aa709ab6b23c69ac9d0b
Jan Greger Hemb <jan.greger@gmail.com>
2014-09-03 23:42:14 +0200

    Just plating around whith SFML

--- projecttower/Source.cpp
+++ projecttower/Source.cpp
@@ -1,21 +1,45 @@
 #include <SFML\Window.hpp>
 #include <SFML\Graphics.hpp>
 #include <vector>
+#include <iostream>
+
+int screenX = 800;
+int screenY = 600;
+
+sf::RenderWindow mainWindow(sf::VideoMode(screenX, screenY), "Greger world");
+
+void setSpriteSize(sf::Sprite * sprite, double x, double y){
+
+	sprite->setScale(x / sprite->getLocalBounds().width, y / sprite->getLocalBounds().height);
+
+}
+
+void drawToScreen(sf::Sprite sprite){
+
+	mainWindow.draw(sprite);
+
+}
+
+void drawToScreen(sf::Sprite * sprite){
+
+	mainWindow.draw(*sprite);
+
+}
 
 
 int main(int argc, char* argv[])
 {
 	int x, y;
-	int screenX, screenY;
 
 	
 
 	x = 20;
 	y = 20;
-	screenX = 800;
-	screenY = 600;
 
-	sf::RenderWindow mainWindow(sf::VideoMode(screenX, screenY), "Greger world");
+	x = y = 2;
+
+
+	
 
 	sf::Texture image;
 	if (!image.loadFromFile("tile_1.png"))
@@ -23,23 +47,47 @@ int main(int argc, char* argv[])
 		// What do when there is no image?!
 	}
 
-	std::vector<sf::Sprite> sprites;
+	std::vector<sf::Sprite *> sprites;
 
-	for (int i = 0; i < x*y; i++)
-	{
-		sprites.push_back(sf::Sprite(image));
-		sprites[i].move(i % x * screenX / x, floor(i / y) * screenY / y);
+	for (int i = 0; i < x*y; i++){
+
+		sprites.push_back(new sf::Sprite(image));
+		sprites[i]->setScale(1.5f, 1.5f);
+		//sprites[i]->setScale(100 / sprites[i].getLocalBounds().width, 100 / sprites[i].getLocalBounds().height);
+		setSpriteSize(sprites[i], 100, 100);
+		sprites[i]->setPosition(i % x * screenX / x, floor(i / y) * screenY / y);
+
 	}
 
-	while (mainWindow.isOpen())
-	{
+	while (mainWindow.isOpen()){
+
 		sf::Event mainEvent;
-		while (mainWindow.pollEvent(mainEvent))
-		{
-			if (mainEvent.type == sf::Event::Closed)
-			{
+		while (mainWindow.pollEvent(mainEvent)){
+
+			if (mainEvent.type == sf::Event::Closed){
+
 				mainWindow.close();
 			}
+
+			if (mainEvent.type == sf::Event::KeyPressed){
+
+				if (mainEvent.key.code == sf::Keyboard::Escape){
+
+					mainWindow.close();
+
+					//sf::Mouse::setPosition(sf::Vector2i(10, 50));
+					//sf::Mouse::setPosition(sf::Vector2i(10, 50), mainWindow);
+
+					std::cout << "the escape key was pressed" << std::endl;
+					std::cout << "control:" << mainEvent.key.control << std::endl;
+					std::cout << "alt:" << mainEvent.key.alt << std::endl;
+					std::cout << "shift:" << mainEvent.key.shift << std::endl;
+					std::cout << "system:" << mainEvent.key.system << std::endl;
+
+				}
+
+			}
+
 		}
 		mainWindow.clear();
 
@@ -48,13 +96,17 @@ int main(int argc, char* argv[])
 		step++;
 		step = step % (x*y);
 
-		printf("%d\n", step);
+		//printf("%d\n", step);
 
 		for (int j = 0; j < step; j++)
 		{
-			mainWindow.draw(sprites[j]);
+			drawToScreen(sprites[j]);
 		}
 
+		sf::CircleShape shape(50);
+		shape.setPosition(0, 100);
+		shape.setFillColor(sf::Color(100, 250, 50));
+		mainWindow.draw(shape);
 
 		/*
 		for (int i = 0; i < x*y; i++)
